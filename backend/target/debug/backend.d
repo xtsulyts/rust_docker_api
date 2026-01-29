b42d0465118823c79cd8862cbdac0b926b7a7e18
xtsulyts <531xtsulyts@gmail.com>
2026-01-29 01:36:49 -0300

    cargo new.

---
+++ backend/target/debug/backend.d
@@ -0,0 +1,1 @@
+/home/xtsulyts/Escritorio/rust_docker_api/backend/target/debug/backend: /home/xtsulyts/Escritorio/rust_docker_api/backend/src/main.rs
---
+++ backend/target/debug/deps/backend-70cd54a9f6ba821c.d
@@ -0,0 +1,5 @@
+/home/xtsulyts/Escritorio/rust_docker_api/backend/target/debug/deps/backend-70cd54a9f6ba821c.d: src/main.rs
+
+/home/xtsulyts/Escritorio/rust_docker_api/backend/target/debug/deps/backend-70cd54a9f6ba821c: src/main.rs
+
+src/main.rs:
